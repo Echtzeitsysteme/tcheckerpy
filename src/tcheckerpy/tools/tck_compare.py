fa57cd4d6f0e97390b5ea6dbd9cb5f8243b421c1
alzeha <Alexander.Lieb@online.de>
2026-03-31 15:54:51 +0200

    Enhance tck_compare function with additional parameters
    Added parameters for output filename and system declaration files, and updated function call arguments accordingly.

--- src/tcheckerpy/tools/tck_compare.py
+++ src/tcheckerpy/tools/tck_compare.py
@@ -10,12 +10,16 @@ def compare(first_sys_decl: str, second_sys_decl: str, relationship: Relationshi
     """
     Checks for bisimilarity of two timed automata.
 
-    :param first_sys_decl: system declaration of first timed automaton
-    :param second_sys_decl: system declaration of second timed automaton
-    :param relationship: relationship to be checked (so far only strong timed bisimilarity is supported)
-    :param block_size: block size for internal computation
-    :param table_size: table size for internal computation
-    :return: result of bisimilarity check, statistics and witness
+    \param output_filename Path to the output file (comparison results)
+    \param first_sysdecl_filename Path to the first system declaration file
+    \param second_sysdecl_filename Path to the second system declaration file
+    \param relationship Type of relationship to check (see tck_compare_relationship_t)
+    \param block_size Pointer to the block size for internal computation (nullptr for default)
+    \param table_size Pointer to the table size for internal computation (nullptr for default)
+    \param starting_state_attributes_first
+    \param starting_state_attributes_second
+    \param inter_constraint
+    \param generate_witness
     """
     
     with tempfile.NamedTemporaryFile(delete=False) as temp_file_first_sysdecl:
@@ -32,7 +36,7 @@ def compare(first_sys_decl: str, second_sys_decl: str, relationship: Relationshi
                   "ctypes.POINTER(ctypes.c_int)", "ctypes.POINTER(ctypes.c_int)", "ctypes.c_bool"],
         has_result=True,
         args=[temp_file_path_first_sysdecl, temp_file_path_second_sysdecl, relationship.value, 
-              block_size, table_size, generate_witness]
+              block_size, table_size, None, None, None, generate_witness]
     )
 
     return "RELATIONSHIP_FULFILLED true" in stats, stats, witness
